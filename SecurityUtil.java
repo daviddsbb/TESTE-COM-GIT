109ba477a27391b56cb80fa0824ce5fdbd1278a8
David <dm6352813@gmail.com>
2025-02-08 00:08:57 -0300

    -a

---
+++ SecurityUtil.java
@@ -0,0 +1,45 @@
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+public class SecurityUtil {
+
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128); 
+        return keyGen.generateKey();
+    }
+
+    public static String encrypt(String strToEncrypt, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(strToEncrypt.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String strToDecrypt, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(strToDecrypt));
+        return new String(decryptedBytes);
+    }
+
+    public static void main(String[] args) {
+        try {
+            String originalString = "123456789";
+            SecretKey secretKey = generateKey();
+
+            System.out.println("Original: " + originalString);
+
+            String encryptedString = encrypt(originalString, secretKey);
+            System.out.println("Criptografado: " + encryptedString);
+
+            String decryptedString = decrypt(encryptedString, secretKey);
+            System.out.println("Descriptografado: " + decryptedString);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
